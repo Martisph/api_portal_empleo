e51c2a2c5c10c3bc74927062c8e82b6d5ee35c98
Stiph Antonio Justo <antoniojstiph@gmail.com>
2024-09-30 19:39:17 -0500

    Agregando la dependencia cookie-parser para las validaciones de cookies y su uso en el inicio de sesion

--- src/config.js
+++ src/config.js
@@ -4,5 +4,6 @@ export const {
   DB_HOST = process.env.DB_HOST,
   DB_PASSWORD = process.env.DB_PASSWORD,
   DB_DATABASE = process.env.DB_DATABASE,
-  DB_PORT = process.env.DB_PORT ?? 3000
+  DB_PORT = process.env.DB_PORT ?? 3000,
+  SECRET_JWS_KEY = 'token_secreto_para_el_funcionamiento_de_la_autentificacion'
 } = process.env
--- src/controllers/autentificacion.js
+++ src/controllers/autentificacion.js
@@ -1,13 +1,20 @@
 import { Usuario } from '../models/usuario.js'
 import { validateCredenciales } from '../schemas/credenciales.js'
+import jwt from 'jsonwebtoken'
+import { SECRET_JWS_KEY } from '../config.js'
 
 export async function auntentificacionController (req, res) {
   const data = validateCredenciales(req.body)
   try {
     const user = await new Usuario(data)
     const login = await user.loginUsuario()
-
-    if (login) return res.status(200).json(login)
+    if (login) {
+      const token = jwt.sign(
+        { id: login.id_usuario, name: login.nombre, email: login.email },
+        SECRET_JWS_KEY,
+        { expiresIn: '5m' })
+      return res.status(200).json({ login, token })
+    }
     return res.status(500).json({ message: ' Internal error ' })
   } catch (e) {
     return res.status(500).json({ message: e.message })
