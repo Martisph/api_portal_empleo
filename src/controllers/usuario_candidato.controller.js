664e1e6bf5be13ca9e90df7e5921e7f5eb1b4f7c
Stiph Antonio Justo <antoniojstiph@gmail.com>
2024-10-22 11:24:15 -0500

    Controlador de registro de usuario candidato ya agregado

---
+++ src/controllers/usuario_candidato.controller.js
@@ -0,0 +1,54 @@
+import { Usuario } from '../models/usuario.js'
+import { Candidato } from '../models/candidato.js'
+import { pool } from '../database/db.js'
+import { validateUsuario, validateEmailUsuario } from '../schemas/usuario.js'
+import { validateCandidato } from '../schemas/candidato.js'
+
+export const postUsuario = async (req, res) => {
+  try {
+    const dataUsuario = validateUsuario(req.body)
+    const dataCandidato = validateCandidato(req.body)
+    if (dataUsuario.success && dataCandidato.success) {
+      console.log(dataUsuario)
+      const email = await Usuario.existsEmail(validateEmailUsuario(req.body))
+      if (email) return res.status(401).json({ error: ' Email ya existe ' })
+    } else {
+      return res.status(400).json({ error: ' Ingreso de datos erroneos ' })
+    }
+
+    await pool.query('BEGIN')
+
+    const usuario = await Usuario.postUsuario(dataUsuario)
+    const candidato = await Candidato.postCandidato(usuario, dataCandidato)
+    await pool.query('COMMIT')
+    console.log(usuario, candidato)
+
+    return res.status(200).json({ ...usuario, ...candidato })
+  } catch (e) {
+    await pool.query('ROLLBACK')
+    console.log(e)
+    return res.status(500).json({ error: e.message })
+  }
+}
+
+export const deleteUsuario = async (req, res) => {
+  try {
+    const usuario = await Usuario.deleteUsuario(req.params)
+    if (usuario) {
+      return res.status(200).json({ message: ' Usuario eliminado ' })
+    }
+    return res.status(404).json({ error: ' Usuario no eliminado ' })
+  } catch (e) {
+    return res.status(500).json({ error: e.message })
+  }
+}
+
+export const putUsuario = async (req, res) => {
+  try {
+    const data = validateUsuario(req.body)
+    const usuario = await Usuario.putUsuario(req.params, data)
+    return res.json(usuario)
+  } catch (e) {
+    res.status(500).json({ error: e.message })
+  }
+}
