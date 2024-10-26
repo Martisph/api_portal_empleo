86ac3554ec9df4544af6e2b438aa32b74244b3fd
Stiph Antonio Justo <antoniojstiph@gmail.com>
2024-10-25 20:31:07 -0500

    agregando controllador de registro de usuario empresa, modificacion de codigo del esquema de empresa

---
+++ src/controllers/usuario_empresa.controller.js
@@ -0,0 +1,52 @@
+import { Usuario } from '../models/usuario.js'
+import { Empresa } from '../models/empresa.js'
+import { pool } from '../database/db.js'
+import { validateUsuario, validateEmailUsuario } from '../schemas/usuario.js'
+import { validateEmpresa } from '../schemas/empresa.js'
+
+export const postUsuario = async (req, res) => {
+  try {
+    console.log(req.body)
+    const dataUsuario = validateUsuario(req.body)
+    const dataEmpresa = validateEmpresa(req.body)
+    if (dataUsuario.success && dataEmpresa.success) {
+      console.log(dataUsuario)
+      const email = await Usuario.existsEmail(validateEmailUsuario(req.body))
+      if (email) return res.status(401).json({ error: ' Email ya existe ' })
+    } else {
+      return res.status(400).json({ error: ' Ingreso de datos erroneos ' })
+    }
+
+    await pool.query('BEGIN')
+    const usuario = await Usuario.postUsuario(dataUsuario)
+    const empresa = await Empresa.postEmpresa(usuario, dataEmpresa)
+
+    await pool.query('COMMIT')
+    return res.status(200).json({ ...usuario, ...empresa })
+  } catch (e) {
+    await pool.query('ROLLBACK')
+    return res.status(500).json({ error: e.message })
+  }
+}
+
+export const deleteUsuario = async (req, res) => {
+  try {
+    const usuario = await Usuario.deleteUsuario(req.params)
+    if (usuario) {
+      return res.status(200).json({ message: ' Usuario eliminado ' })
+    }
+    return res.status(404).json({ error: ' Usuario no eliminado ' })
+  } catch (e) {
+    return res.status(500).json({ error: e.message })
+  }
+}
+
+export const putUsuario = async (req, res) => {
+  try {
+    const data = validateUsuario(req.body)
+    const usuario = await Usuario.putUsuario(req.params, data)
+    return res.json(usuario)
+  } catch (e) {
+    res.status(500).json({ error: e.message })
+  }
+}
--- src/schemas/empresa.js
+++ src/schemas/empresa.js
@@ -4,15 +4,15 @@ const empresaSchema = z.object({
   fk_id_usuario: z.string(),
   nombre: z.string(),
   razon_social: z.string(),
-  descripcion: z.string(),
+  descripcion: z.string().optional(),
   ruc: z.number(),
-  vision: z.string(),
-  mision: z.string(),
-  valores: z.string(),
+  vision: z.string().optional(),
+  mision: z.string().optional(),
+  valores: z.string().optional(),
   sector: z.string(),
-  direccion: z.string(),
-  telefono: z.number(),
-  email: z.string().email()
+  direccion: z.string().optional(),
+  telefono: z.number().optional(),
+  email: z.string().email().optional()
 })
 
 export function validateEmpresa (object) {
